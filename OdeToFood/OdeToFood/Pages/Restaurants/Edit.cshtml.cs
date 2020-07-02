1be9431267e5da91fca265b3c5509abc194d2fba
Asen Penchev <asen.penchev@grafixoft.com>
2020-07-02 13:19:00 -0400

    Building an Edit Form with Tag Helpers

--- OdeToFood/OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using OdeToFood.Core;
 using OdeToFood.Data;
 
@@ -12,15 +10,21 @@ namespace OdeToFood.Pages.Restaurants
     public class EditModel : PageModel
     {
         private readonly IRestaurantData _restaurantData;
+        private readonly IHtmlHelper _htmlHelper;
 
         public Restaurant Restaurant { get; set; }
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
 
-        public EditModel(IRestaurantData restaurantData)
+        public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelper)
         {
             this._restaurantData = restaurantData;
+            _htmlHelper = htmlHelper;
+            this._htmlHelper = htmlHelper;
         }
         public IActionResult OnGet(int restaurantId)
         {
+            Cuisines = this._htmlHelper.GetEnumSelectList<CuisineType>();
+
             Restaurant = this._restaurantData.GetById(restaurantId);
             if (Restaurant == null)
             {
