2ed0cd4fb1477e7686bf3d3b501db8e89a93d9c1
Asen Penchev <asen.penchev@grafixoft.com>
2020-07-05 14:45:51 -0400

    Logging Application Messages

--- OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using OdeToFood.Core;
 using OdeToFood.Data;
 
@@ -11,16 +12,18 @@ namespace OdeToFood.Pages.Restaurants
     {
         private readonly IConfiguration _config;
         private readonly IRestaurantData _restaurantData;
+        private readonly ILogger<ListModel> _logger;
 
         public string Message { get; set; }
         public  IEnumerable<Restaurant> Restaurants { get; set; }
         [BindProperty(SupportsGet = true)]
         public string SearchTerm { get; set; }
 
-        public ListModel(IConfiguration config, IRestaurantData restaurantData)
+        public ListModel(IConfiguration config, IRestaurantData restaurantData, ILogger<ListModel> logger)
         {
             this._config= config;
             this._restaurantData = restaurantData;
+            this._logger = logger;
         }
         
         public void OnGet()
