f105fb576096050abb41afcf12704786a08ebfc2
Asen Penchev <asen.penchev@grafixoft.com>
2020-07-07 15:35:31 -0400

    Download xml version of restaurant

---
+++ OdeToFood/OdeToFood/Pages/Restaurants/Download.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class DownloadModel : PageModel
+    {
+        private readonly IRestaurantData _restaurantData;
+        public Restaurant Restaurant { get; set; }
+
+        public DownloadModel(IRestaurantData restaurantData)
+        {
+            _restaurantData = restaurantData;
+        }
+        public void OnGet(int restaurantId)
+        {
+            Restaurant = this._restaurantData.GetById(restaurantId);
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Restaurant));
+            serializer.Serialize(System.IO.File.Create($"{Restaurant.Name}_{restaurantId}.xml"), Restaurant);
+        }
+    }
+}
